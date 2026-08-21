8239ecb2a010c8a91e229b4a26912fa22d055ff5
Bunnyxdave <mdburhan152004@gmail.com>
2026-08-21 17:02:57 +0530

    js loops

--- javascript/Day4/assignment.js
+++ javascript/Day4/assignment.js
@@ -5,13 +5,42 @@
 // Store the result in a new variable called cleanInput
 // Print both userInput and cleanInput to see the difference
 
+// let userInput = " HeLLo JaVaScRiPt "
+// let clearInput=userInput.trim().toLowerCase()
+// console.log(userInput, clearInput)
+
 // Question 2: You have a variable email with the value " USER@EXAMPLE.COM " . Clean this email by:
 // Removing leading and trailing spaces
 // Converting it to all lowercase
 // Store the result in a variable called formattedEmail
 // Print the result
 
+// let email =" USER@EXAMPLE.COM "
+// let formattedEmail= email.trim().toLocaleLowerCase()
+// console.log(formattedEmail)
+
+
+// Section 2: indexOf() Method
+// Question 1: Create a string sentence = "JavaScript is awesome and JavaScript is fun" . Use the indexOf() method
+// to:
+// Find the position of the word "awesome"
+// Find the position of the first occurrence of "JavaScript"
+// Try to find the word "Python" and see what value you get
+// Print all three results with appropriate labels
+
+// let sentence = "JavaScript is awesome and JavaScript is fun"
+// let awsomeIndex=sentence.indexOf("awesome")
+// let jsIndex=sentence.indexOf("JavaScript")
+// let pyIndex=sentence.indexOf("python")
+
+// console.log(awsomeIndex, jsIndex,pyIndex)
 
 
 
+// Question 2: Create a variable message = "Welcome to coding class" . Write code to:
+// Check if the word "coding" exists in the message using indexOf()
+// If it exists (hint: check if indexOf doesn't return -1), print "Found coding!"
+// If it doesn't exist, print "Coding not found"
+
+let message = "Welcome to coding class"
 
---
+++ javascript/Day5/script.js
@@ -0,0 +1,52 @@
+//DAY 5 OF JS 
+//----LOOPS AND CONTROL FLOW OF THE PROGRAM--------
+
+//Loops allow you to execute a piece of code multiple times without rewriting it. This is called iteration. Instead
+// of writing the same code 10, 100, or 1000 times, you can use a loop to repeat it automatically.
+
+//example1: print numbers from 1-5
+
+// for(let i =1; i<=5; i++){
+//     console.log(i)
+// }
+
+//Example2: print numbers fron 1-15
+
+// for(let i=1; i<=15; i+=2){
+
+//     console.log(i)
+// }
+
+
+//example3: print even numbers from 2-10
+
+// for (let i=2; i<=10; i+=2){
+//     console.log(i)
+// }
+
+//example4: multiples of 5 
+// for(let i=5; i<=50; i+=5){
+//     console.log(i)
+// } 
+
+//example5: print multiplication table of 5
+// for(let i=1;i<=10;i++){
+//     console.log(`5 x ${i} = ${5*i}`)
+// }
+
+//example6:countdown from 10-1
+// for(let i=10; i>=1; i--){
+//     console.log(i)
+// }
+
+
+//-----NESTED FOR LOOPS-----
+
+//example1:
+for(let i=1; i<=3; i++){
+    for(j=1; j<=5; j++){
+            console.log(i)
+    }
+}
+
+
