1575515d936a058fd753951be51c7bf71ac25c55
Bunnyxdave <mdburhan152004@gmail.com>
2026-09-20 21:06:45 +0530

    js day 14 fixed

--- javascript/Day14/script.js
+++ javascript/Day14/script.js
@@ -31,65 +31,86 @@
 //     setTimeout(() => {
 //       if (isDbavailable) {
 //         resolve("completed task 1");
-function registration(username) {
-	const registeredUsers = ["armaan", "Malik", "sajjad", "Ali", "Muqtaar"];
+//       } else {
+//         reject("db is unavailable");
+//       }
+//     }, delay * 1000);
+//   });
+// }
 
-	if (registeredUsers.includes(username)) {
-		throw new Error("user is already registered");
-	}
+// function task2() {
+//   return new Promise((resolve, reject) => {
+//     let delay = Math.floor(Math.random() * 3);
+//     let isDbavailable = Math.floor(Math.random() * 2);
+//     console.log("starting task 2...");
+//     setTimeout(() => {
+//       if (isDbavailable) {
+//         resolve("completed task 2");
+//       } else {
+//         reject("db is unavailable");
+//       }
+//     }, delay * 1000);
+//   });
+// }
 
-	console.log("user is not registered, continuing...");
-	registeredUsers.push(username);
-	return username;
-}
+// function task3() {
+//   return new Promise((resolve, reject) => {
+//     let delay = Math.floor(Math.random() * 3);
+//     let isDbavailable = Math.floor(Math.random() * 2);
+//     console.log("starting task 3...");
+//     setTimeout(() => {
+//       if (isDbavailable) {
+//         resolve("completed task 3");
+//       } else {
+//         reject("db is unavailable");
+//       }
+//     }, delay * 1000);
+//   });
+// }
 
-function verifyingEmail() {
-	return new Promise((resolve) => {
-		const delay = Math.floor(Math.random() * 4);
-		console.log("registration started...");
-		setTimeout(() => resolve("verifying email..."), delay * 1000);
-	});
-}
+// async function runningTask() {
+//   console.log("running all tasks");
+//   try {
+//     let response1 = await task1();
+//     console.log(response1);
 
-function auth() {
-	return new Promise((resolve) => {
-		const delay = Math.floor(Math.random() * 4);
-		setTimeout(() => resolve("authenticating..."), delay * 1000);
-	});
-}
+//     let response2 = await task2();
+//     console.log(response2);
 
-function add() {
-	return new Promise((resolve) => {
-		const delay = Math.floor(Math.random() * 4);
-		setTimeout(() => resolve("adding user to db..."), delay * 1000);
-	});
-}
+//     let response3 = await task3();
+//     console.log(response3);
 
-function addSuccess() {
-	return new Promise((resolve) => {
-		const delay = Math.floor(Math.random() * 4);
-		setTimeout(() => resolve("user added successfully..."), delay * 1000);
-	});
-}
+//     console.log("all tasks completed");
+//   } catch (rejectMsg) {
+//     console.log(rejectMsg);
+//   }
+// }
 
-function welcome(username) {
-	return Promise.resolve(`welcome, ${username}!`);
-}
+// runningTask();
 
-async function flow(user) {
-	try {
-		const newUser = registration(user);
-		console.log(await verifyingEmail());
-		console.log(await auth());
-		console.log(await add());
-		console.log(await addSuccess());
-		console.log(await welcome(newUser));
-	} catch (error) {
-		console.log(error.message);
-	}
-}
+//-------------ASYNC-------------
+//Async functions with Errors
 
-flow("Hassan");
+// async function divide(a,b){
+//     if(b === 0){
+//         throw new Error("Cannot Divide by Zero")
+//     }
+//     else {
+//         return a/b
+//     }
+
+// }
+
+// divide(10,2)
+//     .then(result => console.log('result', result))
+//     .catch( error => console.log("error",error.message))
+
+//     divide(0,0)
+
+//------AWAIT-------------
+
+//What Does await Do?
+// The await keyword literally "pauses" the execution of an async function until a Promise is settled (either
 // resolved or rejected). It can ONLY be used inside an async function.
 
 // example1:
